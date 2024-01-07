bc11a4cebb77e5bba2e65246f92e8eccf685b41f
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-01-07 21:34:51 +0530

    usercontroller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -19,7 +19,14 @@ export const login = async (req, res) => {
     
 }
 export const logout = async (req, res) => {
-    
+    try {
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "Logged out successfully.",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
 }
 export const updateProfile = async (req, res) => {
     try {
