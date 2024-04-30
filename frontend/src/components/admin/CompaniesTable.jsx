5a1457325b75af6bd5c6e1bd3830acc214428fb7
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-04-30 23:51:12 +0530

    CompaniesTable

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -29,7 +29,33 @@ const CompaniesTable = () => {
                         <TableHead className="text-right">Action</TableHead>
                     </TableRow>
                 </TableHeader>
-               
+                <TableBody>
+                    {
+                        filterCompany?.map((company) => (
+                            <tr>
+                                <TableCell>
+                                    <Avatar >
+                                        <AvatarImage src={company.logo}/>
+                                    </Avatar>
+                                </TableCell>
+                                <TableCell className='textcolor text-base'>{company.name}</TableCell>
+                                <TableCell className='textcolor text-base'>{company.createdAt.split("T")[0]}</TableCell>
+                                <TableCell className="text-right cursor-pointer textcolor">
+                                    <Popover>
+                                        <PopoverTrigger><MoreHorizontal /></PopoverTrigger>
+                                        <PopoverContent className="w-32 bg-black">
+                                            <div onClick={()=> navigate(`/admin/companies/${company._id}`)} className='flex items-center gap-2 w-fit cursor-pointer'>
+                                                <Edit2 className='w-4 textcolor' />
+                                                <span className='textcolor'>Edit</span>
+                                            </div>
+                                        </PopoverContent>
+                                    </Popover>
+                                </TableCell>
+                            </tr>
+
+                        ))
+                    }
+                    </TableBody>
             </Table>
         </div>
     )
