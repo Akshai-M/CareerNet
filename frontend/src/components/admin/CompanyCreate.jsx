40905ea43dd6311f92dcf04e7306a9ad21bb797a
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-05-01 23:53:39 +0530

    CompanyCreate

---
+++ frontend/src/components/admin/CompanyCreate.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Navbar from '../shared/Navbar';
+import { Label } from '../ui/label';
+import { Button } from '../ui/button';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { COMPANY_API_END_POINT } from '@/utils/constant';
+import { toast } from 'sonner';
+import { useDispatch } from 'react-redux';
+import { setSingleCompany } from '@/redux/companySlice';
+import { Input } from '../ui/input';
+
+const CompanyCreate = () => {
+    const navigate = useNavigate();
+    const [companyName, setCompanyName] = useState('');
+    const dispatch = useDispatch();
+
+    const registerNewCompany = async () => {
+        try {
+            const res = await axios.post(
+                `${COMPANY_API_END_POINT}/register`,
+                { companyName },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    withCredentials: true,
+                }
+            );
+            if (res?.data?.success) {
+                dispatch(setSingleCompany(res.data.company));
+                toast.success(res.data.message);
+                const companyId = res?.data?.company?._id;
+                navigate(`/admin/companies/${companyId}`);
+            }
+        } catch (error) {
+            toast.error("Company already exists")
+        }
+    };
+
+    return (
+        <div>
+            <Navbar />
+            
+        </div>
+    );
+};
+
+export default CompanyCreate;
