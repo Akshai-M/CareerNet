e0e55bef7ade7cdf55b420f98c84ad7a744696a4
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-05-01 23:53:47 +0530

    CompanyCreate

--- frontend/src/components/admin/CompanyCreate.jsx
+++ frontend/src/components/admin/CompanyCreate.jsx
@@ -41,7 +41,25 @@ const CompanyCreate = () => {
     return (
         <div>
             <Navbar />
-            
+            <div className="max-w-4xl mx-auto">
+                <div className="my-10">
+                    <h1 className="font-bold text-2xl">Your Company Name</h1>
+                    <p className="text-gray-500">
+                        What would you like to give your company name? You can change this later.
+                    </p>
+                </div>
+
+                <Label>Company Name</Label>
+                <Input type="text"
+                    className=" flex my-2 outline-none border-blue-600"
+                    placeholder="JobHunt, Microsoft, etc."
+                    onChange={(e) => setCompanyName(e.target.value)} />
+                
+                <div className="flex items-center gap-2 my-10">
+                    <Button variant="outline" onClick={() => navigate('/admin/companies')}>Cancel</Button>
+                    <Button className='bg-blue-600 hover:bg-blue-400' onClick={registerNewCompany}>Continue</Button>
+                </div>
+            </div>
         </div>
     );
 };
