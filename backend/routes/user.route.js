485faa917b1f4ecf6ae059d122775fe55890e50b
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-12-08 19:23:33 +0530

    userroute

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
  
 const router = express.Router();
 
 router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
+router.route("/logout").get(logout);
+
 export default router;
