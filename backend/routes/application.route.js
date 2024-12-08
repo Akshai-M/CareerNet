d4041eabf08fcd25e098facbc31bbac2c463f20a
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-12-08 20:21:43 +0530

    applicationroute

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
+ 
 const router = express.Router();
 
 router.route("/apply/:id").get(isAuthenticated, applyJob);
 router.route("/get").get(isAuthenticated, getAppliedJobs);
+router.route("/:id/applicants").get(isAuthenticated, getApplicants);
  
 
 export default router;
