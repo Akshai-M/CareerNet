029a7e66c251adabbe40609582bd60662c135f60
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-12-09 19:30:57 +0530

    jobroute

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -1,8 +1,9 @@
 import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 const router = express.Router();
 
 router.route("/post").post(isAuthenticated, postJob);
+router.route("/get").get(isAuthenticated, getAllJobs);
 
 export default router;
-
 
