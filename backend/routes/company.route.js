f5b99e17849218f9e8a73a3b448e9dbce26e0256
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-12-10 20:08:33 +0530

    companyroute

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,8 +1,10 @@
 import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/register").post(isAuthenticated,registerCompany);
+router.route("/get").get(isAuthenticated,getCompany);
 
 export default router;
 
