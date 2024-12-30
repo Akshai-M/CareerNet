883f8e8ea6fc20152afadf03399d7b62ec3ce2d1
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-12-30 10:17:28 +0530

    companymodel

---
+++ backend/models/company.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    description:{
+        type:String, 
+    },
+},{timestamps:true})
+export const Company = mongoose.model("Company", companySchema);
