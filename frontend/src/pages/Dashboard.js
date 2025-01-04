e654839ccc06a6428619ec98c7d3bc474378d09e
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-04 23:24:16 +0530

    Dashboard

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminApproval from "../components/AdminApproval";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <AdminApproval />
+    </div>
+  );
+};
+
+export default Dashboard;
