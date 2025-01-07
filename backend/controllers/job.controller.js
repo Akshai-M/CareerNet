06aa11151693ecddd0e28d14458d06de8ca61dd0
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-08 00:23:00 +0530

    jobcontroller

---
+++ backend/controllers/job.controller.js
@@ -0,0 +1,20 @@
+import { Job } from "../models/job.model.js";
+
+export const postJob = async (req, res) => {
+    
+}
+
+
+export const getAllJobs = async (req, res) => {
+    
+}
+
+
+export const getJobById = async (req, res) => {
+    
+}
+
+
+export const getAdminJobs = async (req, res) => {
+    
+}
