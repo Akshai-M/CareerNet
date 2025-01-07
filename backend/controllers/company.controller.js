a89686b947d889c337c8d586001fc71253f9c3bd
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-08 00:24:00 +0530

    companycontroller

---
+++ backend/controllers/company.controller.js
@@ -0,0 +1,18 @@
+import { Company } from "../models/company.model.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
+
+export const registerCompany = async (req, res) => {
+   
+}
+export const getCompany = async (req, res) => {
+    
+}
+
+
+export const getCompanyById = async (req, res) => {
+    
+}
+export const updateCompany = async (req, res) => {
+    
+}
