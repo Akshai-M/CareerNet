5bbce2c3a67a60c1137cb741ee8ff23436564f16
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-08 00:24:57 +0530

    applicationcontroller

---
+++ backend/controllers/application.controller.js
@@ -0,0 +1,16 @@
+import { Application } from "../models/application.model.js";
+import { Job } from "../models/job.model.js";
+
+export const applyJob = async (req, res) => {
+    
+};
+export const getAppliedJobs = async (req,res) => {
+    
+}
+
+export const getApplicants = async (req,res) => {
+   
+}
+export const updateStatus = async (req,res) => {
+    
+}
