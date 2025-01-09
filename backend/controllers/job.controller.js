c2e8a371bf98d71f2856589a66dfa9b50c216440
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-09 21:30:29 +0530

    jobcontroller

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -16,5 +16,23 @@ export const getJobById = async (req, res) => {
 
 
 export const getAdminJobs = async (req, res) => {
-    
+    try {
+        const adminId = req.id;
+        const jobs = await Job.find({ created_by: adminId }).populate({
+            path:'company',
+            createdAt:-1
+        });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
 }
