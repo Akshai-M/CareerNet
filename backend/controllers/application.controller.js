43ee48f8dbaf53fbd6d038360a549009513b27fe
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-09 21:27:35 +0530

    applicationcontroller

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -5,11 +5,54 @@ export const applyJob = async (req, res) => {
     
 };
 export const getAppliedJobs = async (req,res) => {
-    
+    try {
+        const userId = req.id;
+        const application = await Application.find({applicant:userId}).sort({createdAt:-1}).populate({
+            path:'job',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'company',
+                options:{sort:{createdAt:-1}},
+            }
+        });
+        if(!application){
+            return res.status(404).json({
+                message:"No Applications",
+                success:false
+            })
+        };
+        return res.status(200).json({
+            application,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export const getApplicants = async (req,res) => {
-   
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path:'applications',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'applicant'
+            }
+        });
+        if(!job){
+            return res.status(404).json({
+                message:'Job not found.',
+                success:false
+            })
+        };
+        return res.status(200).json({
+            job, 
+            succees:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 export const updateStatus = async (req,res) => {
     
