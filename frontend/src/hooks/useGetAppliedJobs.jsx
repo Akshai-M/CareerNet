00617a52d7d93a3dcb62e8d7227b9cacef6b51d4
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-11 21:51:40 +0530

    appliedjobs

---
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -0,0 +1,14 @@
+import { setAllAppliedJobs } from "@/redux/jobSlice";
+import { APPLICATION_API_END_POINT } from "@/utils/constant";
+import axios from "axios"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+
+const useGetAppliedJobs = () => {
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        
+    },[])
+};
+export default useGetAppliedJobs;
