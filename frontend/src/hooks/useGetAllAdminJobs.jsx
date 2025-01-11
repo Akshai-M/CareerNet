c9a943fe6cbb5ed1b08fa1a93b3633d76a8b311d
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-11 21:53:37 +0530

    adminpostedjobs

---
+++ frontend/src/hooks/useGetAllAdminJobs.jsx
@@ -0,0 +1,14 @@
+import { setAllAdminJobs } from '@/redux/jobSlice'
+import { JOB_API_END_POINT } from '@/utils/constant'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+const useGetAllAdminJobs = () => {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        
+    },[])
+}
+
+export default useGetAllAdminJobs
