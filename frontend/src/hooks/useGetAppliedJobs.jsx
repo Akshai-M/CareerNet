a66641f8780cf523741e8b457cbe72f7df5c5346
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-12 13:23:32 +0530

    appliedjobs

--- frontend/src/hooks/useGetAppliedJobs.jsx
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -8,7 +8,18 @@ const useGetAppliedJobs = () => {
     const dispatch = useDispatch();
 
     useEffect(()=>{
-        
+        const fetchAppliedJobs = async () => {
+            try {
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
+                console.log(res.data);
+                if(res.data.success){
+                    dispatch(setAllAppliedJobs(res.data.application));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAppliedJobs();
     },[])
 };
 export default useGetAppliedJobs;
