e1a312824ff53651d8cd5a4766ccf78a1c3c58e6
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-12 13:23:03 +0530

    comapanyhook

--- frontend/src/hooks/useGetCompanyById.jsx
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -9,7 +9,15 @@ const useGetCompanyById = (companyId) => {
     const dispatch = useDispatch();
     useEffect(()=>{
         const fetchSingleCompany = async () => {
-            
+            try {
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`,{withCredentials:true});
+                console.log(res.data.company);
+                if(res.data.success){
+                    dispatch(setSingleCompany(res.data.company));
+                }
+            } catch (error) {
+                console.log(error);
+            }
         }
         fetchSingleCompany();
     },[companyId, dispatch])
