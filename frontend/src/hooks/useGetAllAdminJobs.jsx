e16b1b1a4d468458808cb57abe724b0acec4c8ee
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-12 13:24:28 +0530

    adminpostedjobs

--- frontend/src/hooks/useGetAllAdminJobs.jsx
+++ frontend/src/hooks/useGetAllAdminJobs.jsx
@@ -7,7 +7,17 @@ import { useDispatch } from 'react-redux'
 const useGetAllAdminJobs = () => {
     const dispatch = useDispatch();
     useEffect(()=>{
-        
+        const fetchAllAdminJobs = async () => {
+            try {
+                const res = await axios.get(`${JOB_API_END_POINT}/getadminjobs`,{withCredentials:true});
+                if(res.data.success){
+                    dispatch(setAllAdminJobs(res.data.jobs));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAllAdminJobs();
     },[])
 }
 
