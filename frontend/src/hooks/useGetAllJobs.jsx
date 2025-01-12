1542d2a7980115b6cbd2fddc2bfedb4b49e3ab58
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-12 13:23:59 +0530

    alljobs

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -8,7 +8,17 @@ const useGetAllJobs = () => {
     const dispatch = useDispatch();
     const {searchedQuery} = useSelector(store=>store.job);
     useEffect(()=>{
-        
+        const fetchAllJobs = async () => {
+            try {
+                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,{withCredentials:true});
+                if(res.data.success){
+                    dispatch(setAllJobs(res.data.jobs));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAllJobs();
     },[])
 }
 
