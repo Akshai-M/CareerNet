2823cf4eecce7a3c806216335ddad6344e3d8e43
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-13 16:05:29 +0530

    input

---
+++ frontend/src/components/ui/input.jsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Input = React.forwardRef(({ className, type, ...props }, ref) => {
+  return (
+    (<input
+      type={type}
+      className={cn(
+        "flex h-10 w-full rounded-md outline-none border-2  border-input bg-background border-blue-500 mt-1 px-3 py-2 bg-black text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      {...props} />)
+  );
+})
+Input.displayName = "Input"
+
+export { Input }
