10e2a01316a662b4ea981222830af0523410cd01
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-21 12:26:51 +0530

    Herosection

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchedQuery } from "@/redux/jobSlice";
 import { useNavigate } from "react-router-dom";
