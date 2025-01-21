af23a4313a830e6119a34e632cd73e2f20db0171
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-21 12:26:39 +0530

    Herosection

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchedQuery } from "@/redux/jobSlice";
+import { useNavigate } from "react-router-dom";
+const HeroSection = () => {
+
+  const searchJobHandler = () => {
+    dispatch(setSearchedQuery(query));
+    navigate("/browse");
+  };
+
+  return (
+    <div className="text-center">
+      <div className="flex flex-col gap-5 my-10">
+        <span className=" mx-auto px-4 py-2 text-4xl font-bold font-serif  bg-gradient-to-r from-blue-700 via-purple-400 to-[#ee622f] text-transparent bg-clip-text">
+          Hi,{user?.fullname}!
+        </span>
+        
+        
+        
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
