747e30c000d4b8f672463224b7e08e13d52b70e7
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-21 12:24:01 +0530

    Applicants

---
+++ frontend/src/components/admin/Applicants.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import Navbar from '../shared/Navbar'
+import ApplicantsTable from './ApplicantsTable'
+import axios from 'axios';
+import { APPLICATION_API_END_POINT } from '@/utils/constant';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAllApplicants } from '@/redux/applicationSlice';
+
+const Applicants = () => {
+    const params = useParams();
+    const dispatch = useDispatch();
+    const {applicants} = useSelector(store=>store.application);
+
+    useEffect(() => {
+       
+        fetchAllApplicants();
+    }, []);
+    return (
+        <div>
+            <Navbar />
+            
+        </div>
+    )
+}
+
+export default Applicants
