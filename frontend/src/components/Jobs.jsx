52763e9e46927c38c2883e57229e129a959ff0fe
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-23 13:18:22 +0530

    Jobs

---
+++ frontend/src/components/Jobs.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from './shared/Navbar'
+import FilterCard from './FilterCard'
+import Job from './Job';
+import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
+
+const Jobs = () => {
+    const { allJobs, searchedQuery } = useSelector(store => store.job);
+    const [filterJobs, setFilterJobs] = useState(allJobs);
+
+    useEffect(() => {
+       
+    }, [allJobs, searchedQuery]);
+
+    return (
+        <div>
+            <Navbar />
+            <div className='max-w-7xl mx-auto mt-5'>
+                <div className='flex gap-5'>
+                    <div className='w-20%'>
+                        <FilterCard />
+                    </div>
+                    
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Jobs
