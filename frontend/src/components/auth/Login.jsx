72ef488961a0dc87ab35784fa8e665f09f76c4fe
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-22 13:31:59 +0530

    login

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -4,7 +4,6 @@ import { RadioGroup } from "../ui/radio-group";
 import { Button } from "../ui/button";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { USER_API_END_POINT } from "@/utils/constant";
 import { toast } from "sonner";
 import { useDispatch, useSelector } from "react-redux";
 import { setLoading, setUser } from "@/redux/authSlice";
@@ -33,7 +32,7 @@ const Login = () => {
     e.preventDefault();
     try {
       dispatch(setLoading(true));
-      const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+      const res = await axios.post(`${import.meta.env.VITE_USER_API_END_POINT}/login`, input, {
         headers: {
           "Content-Type": "application/json",
         },
