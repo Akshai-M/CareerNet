c67619fecf94756d556e3017cb5a2180ac56a2bf
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-25 00:21:06 +0530

    postjob

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -6,7 +6,6 @@ import { Button } from '../ui/button';
 import { useSelector } from 'react-redux';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
 import axios from 'axios';
-import { JOB_API_END_POINT } from '@/utils/constant';
 import { toast } from 'sonner';
 import { useNavigate } from 'react-router-dom';
 import { Loader2 } from 'lucide-react';
@@ -47,7 +46,7 @@ const PostJob = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const res = await axios.post(`${JOB_API_END_POINT}/post`, input, {
+            const res = await axios.post(`${import.meta.env.VITE_JOB_API_END_POINT}/post`, input, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
