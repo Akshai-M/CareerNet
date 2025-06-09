15c4b3e2a15cc8cc234a80df34e18cac02407386
akshai-m <mylapuramakshaikumar@gmail.com>
2025-06-09 17:17:05 +0530

    refactor:: Profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -34,7 +34,25 @@ const Profile = () => {
 
        
 
-       
+        <div className="my-6">
+          <h1 className="font-semibold text-xl text-gray-800 dark:text-white">
+            Skills
+          </h1>
+          <div className="flex items-center gap-2 my-3">
+            {user?.profile?.skills?.length !== 0 ? (
+              user?.profile?.skills.map((item, index) => (
+                <Badge
+                  key={index}
+                  className="px-4 py-1 text-sm font-medium bg-blue-200 text-blue-800 rounded-full"
+                >
+                  {item}
+                </Badge>
+              ))
+            ) : (
+              <span className="text-gray-600 dark:text-gray-400">NA</span>
+            )}
+          </div>
+        </div>
 
         <div className="grid w-full max-w-sm items-center gap-2 my-5">
           <Label className="text-md text-gray-800 dark:text-white font-bold">
