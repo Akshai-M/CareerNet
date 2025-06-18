00f43f2285d05cb7a8a1f8304aeed37a61c9bddd
akshai-m <mylapuramakshaikumar@gmail.com>
2025-06-18 17:16:09 +0530

    refactor:: index

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: [process.env.FRONTEND_URL,"https://careernet.onrender.com"],
   credentials: true,
 };
 app.use(cors(corsOptions));
