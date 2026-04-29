b6e4c7637295f309afd5f3e14c7060d3c26090ba
Akshai Kumar <mylapuramakshaikumar@gmail.com>
2026-04-29 20:33:41 +0530

    fixed text color for better ui

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -20,7 +20,7 @@ export default function BackgroundLinesDemo() {
       </div>
       
       <p className=""></p>
-      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-300 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         Your Future Awaits <br /><span className="spancolor"> Apply Now!</span>
       </h2>
       <p className="mx-auto  md:text-lg text-[#767676] text-base">
