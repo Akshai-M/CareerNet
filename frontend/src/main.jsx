7391a98e373845dc97b29206a8e3c0be32d4a7c9
Akshai Kumar <mylapuramakshaikumar@gmail.com>
2026-04-29 20:45:49 +0530

    fixed dark mode for better ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
         <Toaster />
     </ThemeProvider>
